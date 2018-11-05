c524e192e33e9c78116b2a9f05ad417f6029c2f2
Felipe Galeote <felipe10gale@gmail.com>
2018-11-04 22:58:12 -0200

    Adiciona as funcionalidades de consultar mesas e atualizar status das mesas

--- src/main/java/com/sancarest/restaurante/endpoint/MesaEndpoint.java
+++ src/main/java/com/sancarest/restaurante/endpoint/MesaEndpoint.java
@@ -1,7 +1,12 @@
 package com.sancarest.restaurante.endpoint;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,17 @@ public class MesaEndpoint {
 	public void adicionarMesa() {
 		repository.save(new Mesa("Dísponivel"));
 	}
+	
+	@GetMapping
+	public List<Mesa> informarMesas(){
+		return repository.findAllByOrderByIdAsc();
+	}
+	
+	@PutMapping
+	public void atualizarMesa(@RequestBody Mesa mesa) {
+		repository.save(mesa);
+	}
+	
+	
 
 }
--- src/main/java/com/sancarest/restaurante/repository/MesaRepository.java
+++ src/main/java/com/sancarest/restaurante/repository/MesaRepository.java
@@ -1,9 +1,11 @@
 package com.sancarest.restaurante.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sancarest.restaurante.model.Mesa;
 
 public interface MesaRepository extends JpaRepository<Mesa, Integer>{
-
+	List<Mesa> findAllByOrderByIdAsc();
 }
